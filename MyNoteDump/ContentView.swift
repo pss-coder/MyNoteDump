847808f9a73594dc15cb82acc4553eb039dc120a
pss-coder <pawandeepse@me.com>
2024-08-01 23:54:48 +0800

    Initial Commit

---
+++ MyNoteDump/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MyNoteDump
+//
+//  Created by Pawandeep Singh on 1/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyNoteDump/MyNoteDumpApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNoteDumpApp.swift
+//  MyNoteDump
+//
+//  Created by Pawandeep Singh on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MyNoteDumpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
